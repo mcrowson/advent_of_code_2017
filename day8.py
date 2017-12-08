6b933101e4c4961070c388d0672f458132e66e18
mcrowson <matthew.d.crowson@gmail.com>
2017-12-08 10:18:17 -0500

    using aocd lib

--- day8.py
+++ day8.py
@@ -1,17 +1,12 @@
-with open('day8_input.txt') as f:
-    operations = f.readlines()
+from collections import defaultdict
+from aocd import data
 
-registers = {}
+registers = defaultdict(int)
 
 highest_val = 0
 # Populate the registers
-for op in operations:
+for op in data.split('\n'):
     name, direc, val, ifs, dep, oper, dep_val = op.split(' ')
-    # Put registers in if they don't exist
-    if name not in registers:
-        registers[name] = 0
-    if dep not in registers:
-        registers[dep] = 0
 
     # Check the condition
     if eval('{} {} {}'.format(registers[dep], oper, dep_val)):
@@ -20,8 +15,7 @@ for op in operations:
         registers[name] += inc
 
         if registers[name] > highest_val:
-            highest_val = registers[name] 
-
+            highest_val = registers[name]
 
 print("P1. Max Value in any register is {}".format(max(registers.values())))
 print("P2. Max value in any register during the program is {}".format(highest_val))
